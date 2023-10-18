8b71a2c0a4607b72d66551bbd80b3a60b6dafaf8
Mael Kerichard <38262536+Pixselve@users.noreply.github.com>
2023-10-18 11:39:59 +0200

    Working on the grammar

--- mini-mecha-code/src/language/mini-mecha-code-validator.ts
+++ mini-mecha-code/src/language/mini-mecha-code-validator.ts
@@ -1,5 +1,5 @@
-import type { ValidationAcceptor, ValidationChecks } from 'langium';
-import type { MiniMechaCodeAstType, Person } from './generated/ast.js';
+import type { ValidationChecks } from 'langium';
+import type { MiniMechaCodeAstType } from './generated/ast.js';
 import type { MiniMechaCodeServices } from './mini-mecha-code-module.js';
 
 /**
@@ -9,7 +9,7 @@ export function registerValidationChecks(services: MiniMechaCodeServices) {
     const registry = services.validation.ValidationRegistry;
     const validator = services.validation.MiniMechaCodeValidator;
     const checks: ValidationChecks<MiniMechaCodeAstType> = {
-        Person: validator.checkPersonStartsWithCapital
+        // Person: validator.checkPersonStartsWithCapital
     };
     registry.register(checks, validator);
 }
@@ -19,13 +19,13 @@ export function registerValidationChecks(services: MiniMechaCodeServices) {
  */
 export class MiniMechaCodeValidator {
 
-    checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
-        if (person.name) {
-            const firstChar = person.name.substring(0, 1);
-            if (firstChar.toUpperCase() !== firstChar) {
-                accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
-            }
-        }
-    }
+    // checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
+    //     if (person.name) {
+    //         const firstChar = person.name.substring(0, 1);
+    //         if (firstChar.toUpperCase() !== firstChar) {
+    //             accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
+    //         }
+    //     }
+    // }
 
 }
