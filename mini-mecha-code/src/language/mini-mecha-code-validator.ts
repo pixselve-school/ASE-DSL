07c73af93fdb739dc9ba5053e0348cb9d797d1bd
Cody Adam <cody.ad4@gmail.com>
2023-10-18 10:04:25 +0200

    add langium

---
+++ mini-mecha-code/src/language/mini-mecha-code-validator.ts
@@ -0,0 +1,31 @@
+import type { ValidationAcceptor, ValidationChecks } from 'langium';
+import type { MiniMechaCodeAstType, Person } from './generated/ast.js';
+import type { MiniMechaCodeServices } from './mini-mecha-code-module.js';
+
+/**
+ * Register custom validation checks.
+ */
+export function registerValidationChecks(services: MiniMechaCodeServices) {
+    const registry = services.validation.ValidationRegistry;
+    const validator = services.validation.MiniMechaCodeValidator;
+    const checks: ValidationChecks<MiniMechaCodeAstType> = {
+        Person: validator.checkPersonStartsWithCapital
+    };
+    registry.register(checks, validator);
+}
+
+/**
+ * Implementation of custom validations.
+ */
+export class MiniMechaCodeValidator {
+
+    checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
+        if (person.name) {
+            const firstChar = person.name.substring(0, 1);
+            if (firstChar.toUpperCase() !== firstChar) {
+                accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
+            }
+        }
+    }
+
+}
---
+++ mini-mecha-code/src/static/setup.js
@@ -0,0 +1,31 @@
+import { MonacoEditorLanguageClientWrapper } from './monaco-editor-wrapper/index.js';
+import { buildWorkerDefinition } from "./monaco-editor-workers/index.js";
+import monarchSyntax from "./syntaxes/mini-mecha-code.monarch.js";
+
+buildWorkerDefinition('./monaco-editor-workers/workers', new URL('', window.location.href).href, false);
+
+MonacoEditorLanguageClientWrapper.addMonacoStyles('monaco-editor-styles');
+
+const client = new MonacoEditorLanguageClientWrapper();
+const editorConfig = client.getEditorConfig();
+editorConfig.setMainLanguageId('mini-mecha-code');
+
+editorConfig.setMonarchTokensProvider(monarchSyntax);
+
+editorConfig.setMainCode(`// Mini Mecha Code is running in the web!`);
+
+editorConfig.theme = 'vs-dark';
+editorConfig.useLanguageClient = true;
+editorConfig.useWebSocket = false;
+
+const workerURL = new URL('./mini-mecha-code-server-worker.js', import.meta.url);
+console.log(workerURL.href);
+
+const lsWorker = new Worker(workerURL.href, {
+    type: 'classic',
+    name: 'MiniMechaCode Language Server'
+});
+client.setWorker(lsWorker);
+
+// keep a reference to a promise for when the editor is finished starting, we'll use this to setup the canvas on load
+const startingPromise = client.startEditor(document.getElementById("monaco-editor-root"));
