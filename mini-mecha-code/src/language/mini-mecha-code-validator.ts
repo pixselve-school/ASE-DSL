b9534d8766d17f28082691d59d4fdb134dcc05d1
Mael Kerichard <38262536+Pixselve@users.noreply.github.com>
2023-11-22 10:42:24 +0100

    Refactor MiniMechaCodeValidator for better accuracy
    Refactoring of the MiniMechaCodeValidator class enhances the validation of DefVariable and DefFunction by guarding against redefinition in the same scope. The changes improve the reliability of the validator and prevents potential bugs associated with variable and function redefinition. Unused imports were also removed for better code tidiness and maintainability.

--- mini-mecha-code/src/language/mini-mecha-code-validator.ts
+++ mini-mecha-code/src/language/mini-mecha-code-validator.ts
@@ -2,7 +2,9 @@ import type { ValidationAcceptor, ValidationChecks } from "langium";
 import {
   type DefFunction,
   type DefVariable,
-  type FunctionCall, Loop,
+  type FunctionCall,
+  FunctionParameter,
+  Loop,
   type MiniMechaCodeAstType,
   type Model,
   type RefVariable,
@@ -10,146 +12,134 @@ import {
 } from "./generated/ast.js";
 import type { MiniMechaCodeServices } from "./mini-mecha-code-module.js";
 import getModel from "../utils/getModel.js";
-import {isDefFunction, isDefVariable, isLoop} from "./generated/ast.js";
+import { isDefFunction, isDefVariable, isLoop } from "./generated/ast.js";
 
 /**
  * Register custom validation checks.
  */
 export function registerValidationChecks(services: MiniMechaCodeServices) {
-    const registry = services.validation.ValidationRegistry;
-    const validator = services.validation.MiniMechaCodeValidator;
-    const checks: ValidationChecks<MiniMechaCodeAstType> = {
-        DefVariable: validator.checkDefVariableIsNotAlreadyDefined,
-        DefFunction: validator.checkFunctionIsNotAlreadyDefined,
-    };
-    registry.register(checks, validator);
+  const registry = services.validation.ValidationRegistry;
+  const validator = services.validation.MiniMechaCodeValidator;
+  const checks: ValidationChecks<MiniMechaCodeAstType> = {
+    DefVariable: validator.checkDefVariableIsNotAlreadyDefined,
+    DefFunction: validator.checkFunctionIsNotAlreadyDefined,
+  };
+  registry.register(checks, validator);
 }
 
 /**
  * Implementation of custom validations.
  */
 export class MiniMechaCodeValidator {
+  checkDefVariableIsNotAlreadyDefined(
+    defVariable: DefVariable,
+    accept: ValidationAcceptor,
+  ): void {
+    const parent = defVariable.$container;
 
-    checkDefVariableIsNotAlreadyDefined(defVariable: DefVariable, accept: ValidationAcceptor): void {
-        const parent = defVariable.$container;
-
-        if (isLoop(parent)) {
-            const loop = parent as Loop;
-            for (let statement of loop.statements) {
-                if (statement === defVariable) {
-                    break;
-                }
-                if (isDefVariable(statement)) {
-                    const defVar = statement as DefVariable;
-                    if (defVar.name === defVariable.name) {
-                        accept("error", `Cannot redeclare block-scoped variable ${defVariable.name}.`, {
-                            code: "variable.alreadyDefined",
-                            node: defVariable,
-                            property: "name"
-                        })
-                    }
-                }
-            }
-            return;
+    if (isLoop(parent)) {
+      const loop = parent as Loop;
+      for (let statement of loop.statements) {
+        if (statement === defVariable) {
+          break;
         }
-        if (isDefFunction(parent)) {
-            const functionDef = parent as DefFunction;
-            if (functionDef.parameters.find(param => param.name === defVariable.name)) {
-                accept("error", `Duplicated identifier ${defVariable.name}.`, {
-                    code: "variable.alreadyDefined",
-                    node: defVariable,
-                    property: "name",
-                })
-            }
-            for (let statement of functionDef.statements) {
-                if (statement === defVariable) {
-                    break;
-                }
-                if (isDefVariable(statement)) {
-                    const defVar = statement as DefVariable;
-                    if (defVar.name === defVariable.name) {
-                        accept("error", `Cannot redeclare block-scoped variable ${defVariable.name}.`, {
-                            code: "variable.alreadyDefined",
-                            node: defVariable,
-                            property: "name",
-                        })
-                    }
-                }
-            }
-            return;
+        if (isDefVariable(statement)) {
+          const defVar = statement as DefVariable;
+          if (defVar.name === defVariable.name) {
+            accept(
+              "error",
+              `Cannot redeclare block-scoped variable ${defVariable.name}.`,
+              {
+                code: "variable.alreadyDefined",
+                node: defVariable,
+                property: "name",
+              },
+            );
+          }
         }
-        return;
+      }
+      return;
     }
-
-    checkFunctionIsNotAlreadyDefined(functionDef: DefFunction, accept: ValidationAcceptor): void {
-        const model = getModel(functionDef.$container);
-        if (!model) {
-            return;
+    if (isDefFunction(parent)) {
+      const functionDef = parent as DefFunction;
+      if (
+        functionDef.parameters.find((param) => param.name === defVariable.name)
+      ) {
+        accept("error", `Duplicated identifier ${defVariable.name}.`, {
+          code: "variable.alreadyDefined",
+          node: defVariable,
+          property: "name",
+        });
+      }
+      for (let statement of functionDef.statements) {
+        if (statement === defVariable) {
+          break;
         }
-        for (let statement of model.statements) {
-            if (statement === functionDef) {
-                break;
-            }
-            if (isDefFunction(statement)) {
-                const defFunction = statement as DefFunction;
-                if (defFunction.name === functionDef.name) {
-                    accept("error", `Cannot redeclare function ${functionDef.name}.`, {
-                        code: "function.alreadyDefined",
-                        node: functionDef,
-                        property: "name",
-                    })
-                }
-            }
+        if (isDefVariable(statement)) {
+          const defVar = statement as DefVariable;
+          if (defVar.name === defVariable.name) {
+            accept(
+              "error",
+              `Cannot redeclare block-scoped variable ${defVariable.name}.`,
+              {
+                code: "variable.alreadyDefined",
+                node: defVariable,
+                property: "name",
+              },
+            );
+          }
         }
+      }
+      return;
     }
+    return;
+  }
 
-  checkRefVariableIsUsingDefinedVariable(
-    refVariables: RefVariable,
-    accept: ValidationAcceptor
-  ): void {}
-
-  checkFunctionCallIsUsingDefinedFunction(
-    functionCall: FunctionCall,
-    accept: ValidationAcceptor
-  ): void {}
-
-  checkModelHasOnlyFunctionDefinition(
-    model: Model,
-    accept: ValidationAcceptor
-  ): void {}
-
-  checkFunctionAreOnlyDeclaredInModel(
+  checkFunctionIsNotAlreadyDefined(
     functionDef: DefFunction,
-    accept: ValidationAcceptor
-  ): void {}
+    accept: ValidationAcceptor,
+  ): void {
+    const model = getModel(functionDef.$container);
+    if (!model) {
+      return;
+    }
+    for (let statement of model.statements) {
+      if (statement === functionDef) {
+        break;
+      }
+      if (isDefFunction(statement)) {
+        const defFunction = statement as DefFunction;
+        if (defFunction.name === functionDef.name) {
+          accept("error", `Cannot redeclare function ${functionDef.name}.`, {
+            code: "function.alreadyDefined",
+            node: functionDef,
+            property: "name",
+          });
+        }
+      }
+    }
+  }
 
   checkThatAllFunctionParametersAreUnique(
-    functionDef: DefFunction,
-    accept: ValidationAcceptor
+    parameter: FunctionParameter,
+    accept: ValidationAcceptor,
   ): void {
-    const names = functionDef.parameters.map((parameter) => parameter.name);
-    const uniqueNames = new Set(names);
-    if (names.length !== uniqueNames.size) {
-      accept("error", "Duplicate parameter with same name", {
-        node: functionDef,
-        property: "parameters",
-      });
-    }
+    const functionDef = parameter.$container;
   }
 
   checkThatDefVariableIsUsingTheCorrectType(
     defVariable: DefVariable,
-    accept: ValidationAcceptor
+    accept: ValidationAcceptor,
   ): void {}
 
   checkThatFunctionCallIsUsingCorrectType(
     functionCall: FunctionCall,
-    accept: ValidationAcceptor
+    accept: ValidationAcceptor,
   ): void {}
 
   checkThatFunctionCallIsUsingCorrectNumberOfParameters(
     functionCall: FunctionCall,
-    accept: ValidationAcceptor
+    accept: ValidationAcceptor,
   ): void {
     const model = getModel(functionCall.$container);
     if (!model) {
@@ -158,27 +148,25 @@ export class MiniMechaCodeValidator {
         property: "name",
       });
     }
-
-
   }
 
   checkThatVariableAssignmentIsUsingCorrectType(
     varAssignment: VarAssignment,
-    accept: ValidationAcceptor
+    accept: ValidationAcceptor,
   ): void {}
 
   checkThatConversionAreUsingCorrectType(
     varAssignment: VarAssignment,
-    accept: ValidationAcceptor
+    accept: ValidationAcceptor,
   ): void {}
 
   checkThatForwardIsUsingCorrectType(
     varAssignment: VarAssignment,
-    accept: ValidationAcceptor
+    accept: ValidationAcceptor,
   ): void {}
 
   checkThatClockIsUsingCorrectType(
     varAssignment: VarAssignment,
-    accept: ValidationAcceptor
+    accept: ValidationAcceptor,
   ): void {}
 }
