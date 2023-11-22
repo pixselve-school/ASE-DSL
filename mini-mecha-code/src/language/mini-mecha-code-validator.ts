7511816cfcc46d3572bd70ad02d6f08a51af562e
Mael Kerichard <38262536+Pixselve@users.noreply.github.com>
2023-11-22 10:37:52 +0100

    Improve code validation logic for Mini Mecha language
    Introduced better code validation for DefVariable and DefFunction in Mini Mecha language. The improved validation ensures that a variable or function is not redefined within the same scope. These changes improve code quality and prevent bugs related to variable and function redefinition. Also, removed unused import statements to keep the code clean and easy to maintain.

--- mini-mecha-code/src/language/mini-mecha-code-validator.ts
+++ mini-mecha-code/src/language/mini-mecha-code-validator.ts
@@ -1,9 +1,8 @@
 import type { ValidationAcceptor, ValidationChecks } from "langium";
 import {
-  isModel,
   type DefFunction,
   type DefVariable,
-  type FunctionCall,
+  type FunctionCall, Loop,
   type MiniMechaCodeAstType,
   type Model,
   type RefVariable,
@@ -11,29 +10,98 @@ import {
 } from "./generated/ast.js";
 import type { MiniMechaCodeServices } from "./mini-mecha-code-module.js";
 import getModel from "../utils/getModel.js";
+import {isDefFunction, isDefVariable, isLoop} from "./generated/ast.js";
 
 /**
  * Register custom validation checks.
  */
 export function registerValidationChecks(services: MiniMechaCodeServices) {
-  const registry = services.validation.ValidationRegistry;
-  const validator = services.validation.MiniMechaCodeValidator;
-  const checks: ValidationChecks<MiniMechaCodeAstType> = {
-    // Person: validator.checkPersonStartsWithCapital
-    // RefVariable: validator.checkRefVariableIsUsingDefinedVariable,
-    // DefVariable: validator.checkDefVariableIsNotAlreadyDefined
-  };
-  registry.register(checks, validator);
+    const registry = services.validation.ValidationRegistry;
+    const validator = services.validation.MiniMechaCodeValidator;
+    const checks: ValidationChecks<MiniMechaCodeAstType> = {
+        DefVariable: validator.checkDefVariableIsNotAlreadyDefined,
+        DefFunction: validator.checkFunctionIsNotAlreadyDefined,
+    };
+    registry.register(checks, validator);
 }
 
 /**
  * Implementation of custom validations.
  */
 export class MiniMechaCodeValidator {
-  checkDefVariableIsNotAlreadyDefined(
-    defVariable: DefVariable,
-    accept: ValidationAcceptor
-  ): void {}
+
+    checkDefVariableIsNotAlreadyDefined(defVariable: DefVariable, accept: ValidationAcceptor): void {
+        const parent = defVariable.$container;
+
+        if (isLoop(parent)) {
+            const loop = parent as Loop;
+            for (let statement of loop.statements) {
+                if (statement === defVariable) {
+                    break;
+                }
+                if (isDefVariable(statement)) {
+                    const defVar = statement as DefVariable;
+                    if (defVar.name === defVariable.name) {
+                        accept("error", `Cannot redeclare block-scoped variable ${defVariable.name}.`, {
+                            code: "variable.alreadyDefined",
+                            node: defVariable,
+                            property: "name"
+                        })
+                    }
+                }
+            }
+            return;
+        }
+        if (isDefFunction(parent)) {
+            const functionDef = parent as DefFunction;
+            if (functionDef.parameters.find(param => param.name === defVariable.name)) {
+                accept("error", `Duplicated identifier ${defVariable.name}.`, {
+                    code: "variable.alreadyDefined",
+                    node: defVariable,
+                    property: "name",
+                })
+            }
+            for (let statement of functionDef.statements) {
+                if (statement === defVariable) {
+                    break;
+                }
+                if (isDefVariable(statement)) {
+                    const defVar = statement as DefVariable;
+                    if (defVar.name === defVariable.name) {
+                        accept("error", `Cannot redeclare block-scoped variable ${defVariable.name}.`, {
+                            code: "variable.alreadyDefined",
+                            node: defVariable,
+                            property: "name",
+                        })
+                    }
+                }
+            }
+            return;
+        }
+        return;
+    }
+
+    checkFunctionIsNotAlreadyDefined(functionDef: DefFunction, accept: ValidationAcceptor): void {
+        const model = getModel(functionDef.$container);
+        if (!model) {
+            return;
+        }
+        for (let statement of model.statements) {
+            if (statement === functionDef) {
+                break;
+            }
+            if (isDefFunction(statement)) {
+                const defFunction = statement as DefFunction;
+                if (defFunction.name === functionDef.name) {
+                    accept("error", `Cannot redeclare function ${functionDef.name}.`, {
+                        code: "function.alreadyDefined",
+                        node: functionDef,
+                        property: "name",
+                    })
+                }
+            }
+        }
+    }
 
   checkRefVariableIsUsingDefinedVariable(
     refVariables: RefVariable,
@@ -51,11 +119,6 @@ export class MiniMechaCodeValidator {
   ): void {}
 
   checkFunctionAreOnlyDeclaredInModel(
-    functionDef: DefFunction,
-    accept: ValidationAcceptor
-  ): void {}
-
-  checkFunctionIsNotAlreadyDefined(
     functionDef: DefFunction,
     accept: ValidationAcceptor
   ): void {}
@@ -96,7 +159,7 @@ export class MiniMechaCodeValidator {
       });
     }
 
-    
+
   }
 
   checkThatVariableAssignmentIsUsingCorrectType(
